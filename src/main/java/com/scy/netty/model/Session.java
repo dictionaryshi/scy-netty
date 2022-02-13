54fde90d3fc8e2c21bfce6e96e6293ccf545aa14
史春阳 <shichunyang02@meituan.com>
2022-02-13 23:24:42 +0800

    Session.java

---
+++ src/main/java/com/scy/netty/model/Session.java
@@ -0,0 +1,20 @@
+package com.scy.netty.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author : shichunyang
+ * Date    : 2022/2/13
+ * Time    : 11:20 下午
+ * ---------------------------------------
+ * Desc    : Session
+ */
+@Getter
+@Setter
+@ToString
+public class Session {
+
+    private String userId;
+}
