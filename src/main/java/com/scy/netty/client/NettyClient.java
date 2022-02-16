7a6d1cb1cf9d7c1b7c1df6267f6439394ab3229e
史春阳 <shichunyang02@meituan.com>
2022-02-16 14:14:01 +0800

    NettyClient.java

--- src/main/java/com/scy/netty/client/NettyClient.java
+++ src/main/java/com/scy/netty/client/NettyClient.java
@@ -3,10 +3,15 @@ package com.scy.netty.client;
 import com.scy.core.ObjectUtil;
 import com.scy.core.exception.BusinessException;
 import com.scy.core.format.MessageUtil;
+import com.scy.core.format.NumberUtil;
+import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author : shichunyang
@@ -26,6 +31,8 @@ public class NettyClient extends AbstractConnectClient {
     private volatile boolean closed = Boolean.FALSE;
 
     private volatile Channel channel;
+
+    public static final int MAX_RETRY = 3;
 
     @Override
     public void init(String address, ClientConfig clientConfig) throws Exception {
@@ -61,5 +68,29 @@ public class NettyClient extends AbstractConnectClient {
         if (!ObjectUtil.isNull(workerGroup)) {
             workerGroup.shutdownGracefully().syncUninterruptibly();
         }
+    }
+
+    public void connect(Bootstrap bootstrap, String host, int port, int retry) {
+        if (closed) {
+            return;
+        }
+
+        try {
+            channel = bootstrap.connect(host, port).sync().channel();
+        } catch (Exception e) {
+            log.error(MessageUtil.format("netty client connect fail", e, "host", host, "port", port));
+        }
+
+        if (Objects.equals(retry, NumberUtil.ZERO.intValue())) {
+            log.info(MessageUtil.format("netty client reconnect fail", "host", host, "port", port));
+            return;
+        }
+
+        int order = (MAX_RETRY - retry) + 1;
+        log.info(MessageUtil.format("netty client reconnect", "host", host, "port", port, "order", order));
+
+        // 重连间隔
+        int delay = 1 << order;
+        bootstrap.config().group().schedule(() -> connect(bootstrap, host, port, retry - 1), delay, TimeUnit.SECONDS);
     }
 }
