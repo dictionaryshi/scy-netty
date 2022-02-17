e52d652f91e4a853cf99e9ac44d20829d75be257
史春阳 <shichunyang02@meituan.com>
2022-02-17 21:24:24 +0800

    RpcResponseFuture.java

---
+++ src/main/java/com/scy/netty/rpc/RpcResponseFuture.java
@@ -0,0 +1,104 @@
+package com.scy.netty.rpc;
+
+import com.scy.core.exception.BusinessException;
+import com.scy.core.format.MessageUtil;
+import com.scy.netty.model.rpc.RpcRequest;
+import com.scy.netty.model.rpc.RpcResponse;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author : shichunyang
+ * Date    : 2022/2/17
+ * Time    : 8:35 下午
+ * ---------------------------------------
+ * Desc    : RpcResponseFuture
+ */
+public class RpcResponseFuture<T> implements Future<RpcResponse<T>> {
+
+    private volatile Throwable throwable;
+
+    private volatile RpcRequest rpcRequest;
+
+    private volatile RpcResponse<T> rpcResponse;
+
+    private volatile boolean cancelled = Boolean.FALSE;
+
+    private volatile boolean done = Boolean.FALSE;
+
+    private final Object lock = new Object();
+
+    public Throwable getThrowable() {
+        return throwable;
+    }
+
+    public void setThrowable(Throwable throwable) {
+        this.throwable = throwable;
+    }
+
+    public RpcRequest getRpcRequest() {
+        return rpcRequest;
+    }
+
+    public void setRpcRequest(RpcRequest rpcRequest) {
+        this.rpcRequest = rpcRequest;
+    }
+
+    public RpcResponse<T> getRpcResponse() {
+        return rpcResponse;
+    }
+
+    public void setRpcResponse(RpcResponse<T> rpcResponse) {
+        this.rpcResponse = rpcResponse;
+
+        synchronized (lock) {
+            done = Boolean.TRUE;
+            lock.notifyAll();
+        }
+    }
+
+    @Override
+    public boolean cancel(boolean mayInterruptIfRunning) {
+        cancelled = Boolean.TRUE;
+
+        synchronized (lock) {
+            done = Boolean.TRUE;
+            lock.notifyAll();
+        }
+
+        return cancelled;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public boolean isDone() {
+        return done;
+    }
+
+    @Override
+    public RpcResponse<T> get() throws InterruptedException, ExecutionException {
+        throw new BusinessException("prohibited methods");
+    }
+
+    @Override
+    public RpcResponse<T> get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+        if (!done) {
+            synchronized (lock) {
+                long timeoutMillis = (TimeUnit.MILLISECONDS == unit) ? timeout : TimeUnit.MILLISECONDS.convert(timeout, unit);
+                lock.wait(timeoutMillis);
+            }
+        }
+
+        if (!done) {
+            throw new BusinessException(MessageUtil.format("rpc timeout", "rpcRequest", rpcRequest));
+        }
+        return rpcResponse;
+    }
+}
