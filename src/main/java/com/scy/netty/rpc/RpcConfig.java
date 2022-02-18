1d11ec9f09cfcc480b28960860f9219fdbaec243
史春阳 <shichunyang02@meituan.com>
2022-02-18 13:31:19 +0800

    RpcConfig.java

--- src/main/java/com/scy/netty/rpc/RpcConfig.java
+++ src/main/java/com/scy/netty/rpc/RpcConfig.java
@@ -1,5 +1,8 @@
 package com.scy.netty.rpc;
 
+import com.scy.netty.rpc.provider.Provider;
+import org.springframework.context.annotation.Bean;
+
 /**
  * @author : shichunyang
  * Date    : 2022/2/18
@@ -8,4 +11,9 @@ package com.scy.netty.rpc;
  * Desc    : RpcConfig
  */
 public class RpcConfig {
+
+    @Bean
+    public Provider provider() {
+        return new Provider();
+    }
 }
