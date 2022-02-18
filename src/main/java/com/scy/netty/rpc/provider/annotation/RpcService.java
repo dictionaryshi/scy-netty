87f0420192b42f049d9dc0658d7ca8e5cc8d22c2
史春阳 <shichunyang02@meituan.com>
2022-02-18 13:18:02 +0800

    RpcService.java

---
+++ src/main/java/com/scy/netty/rpc/provider/annotation/RpcService.java
@@ -0,0 +1,35 @@
+package com.scy.netty.rpc.provider.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author : shichunyang
+ * Date    : 2022/2/18
+ * Time    : 1:10 下午
+ * ---------------------------------------
+ * Desc    : RpcService
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RpcService {
+
+    /**
+     * 版本
+     */
+    String version();
+
+    /**
+     * 核心线程数
+     */
+    int corePoolSize();
+
+    /**
+     * 最大线程数
+     */
+    int maximumPoolSize();
+
+    /**
+     * 队列数
+     */
+    int queueSize();
+}
