de6a8a3bad8234bf33c53908e64b049d3f244e12
史春阳 <shichunyang02@meituan.com>
2022-02-18 13:26:14 +0800

    Provider.java

---
+++ src/main/java/com/scy/netty/rpc/provider/Provider.java
@@ -0,0 +1,11 @@
+package com.scy.netty.rpc.provider;
+
+/**
+ * @author : shichunyang
+ * Date    : 2022/2/18
+ * Time    : 1:25 下午
+ * ---------------------------------------
+ * Desc    : Provider
+ */
+public class Provider {
+}
