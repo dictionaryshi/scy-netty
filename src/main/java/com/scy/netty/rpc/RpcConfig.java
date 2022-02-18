3769c9007133481272394b09e6a58bd9ae8ab9f3
史春阳 <shichunyang02@meituan.com>
2022-02-18 13:29:03 +0800

    RpcConfig.java

---
+++ src/main/java/com/scy/netty/rpc/RpcConfig.java
@@ -0,0 +1,11 @@
+package com.scy.netty.rpc;
+
+/**
+ * @author : shichunyang
+ * Date    : 2022/2/18
+ * Time    : 1:27 下午
+ * ---------------------------------------
+ * Desc    : RpcConfig
+ */
+public class RpcConfig {
+}
