ea1d5d9eef3c489dea4411ebfb241fe063525708
史春阳 <shichunyang02@meituan.com>
2022-03-04 11:22:22 +0800

    RpcRequestHandler.java

---
+++ src/main/java/com/scy/netty/client/handler/RpcResponseHandler.java
@@ -0,0 +1,26 @@
+package com.scy.netty.client.handler;
+
+import com.scy.netty.model.rpc.RpcResponse;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * @author : shichunyang
+ * Date    : 2022/3/4
+ * Time    : 11:05 上午
+ * ---------------------------------------
+ * Desc    : RpcResponseHandler
+ */
+@ChannelHandler.Sharable
+public class RpcResponseHandler extends SimpleChannelInboundHandler<RpcResponse<?>> {
+
+    public static final RpcResponseHandler INSTANCE = new RpcResponseHandler();
+
+    private RpcResponseHandler() {
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, RpcResponse<?> rpcResponse) throws Exception {
+    }
+}
