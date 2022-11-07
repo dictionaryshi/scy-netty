d7b019a004110f682f436e6d334ac5dd3a5e0ff1
史春阳 <shichunyang02@meituan.com>
2022-11-07 15:04:00 +0800

    MqActiveInfo.java

--- src/main/java/com/scy/netty/mq/ConsumerThread.java
+++ src/main/java/com/scy/netty/mq/ConsumerThread.java
@@ -1,10 +1,14 @@
 package com.scy.netty.mq;
 
+import com.scy.core.CollectionUtil;
 import com.scy.core.UUIDUtil;
 import com.scy.zookeeper.ZkClient;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.TreeSet;
 
 /**
  * @author : shichunyang
@@ -37,6 +41,29 @@ public class ConsumerThread implements Runnable {
         this.mqConsumer = mqConsumer;
         this.consumer = mqConsumer.getClass().getAnnotation(Consumer.class);
         this.uuid = UUIDUtil.uuid();
+    }
+
+    public MqActiveInfo getMqActiveInfo() {
+        List<String> groups = zkClient.getChildren("/mq/".concat(consumer.topic()));
+        if (CollectionUtil.isEmpty(groups)) {
+            return null;
+        }
+
+        TreeSet<String> groupSet = new TreeSet<>(groups);
+
+        int rank = -1;
+        for (String group : groupSet) {
+            rank++;
+            if (group.equals(consumer.group().concat("_").concat(uuid))) {
+                break;
+            }
+        }
+
+        if (rank == -1) {
+            return null;
+        }
+
+        return new MqActiveInfo(rank, groupSet.size(), groupSet.toString());
     }
 
     @Override
